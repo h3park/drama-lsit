4d2b0e846f50fa411b143c49eea8af19a4e3d38a
h3park <h3park@ucsd.edu>
2020-12-01 10:54:15 -0800

    [fixed] wrong import name has been fixed

--- src/components/index-list/index-list.component.jsx
+++ src/components/index-list/index-list.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './index-list.style.css'
+import {Index} from '../index/index.component'
 
 export const IndexList = (props) => {
     return(
         <div className='index-list'>
-            {props.dramas.map(drama => <Card key={drama.id} drama={drama}/>)}
+            {props.dramas.map(drama => <Index key={drama.id} drama={drama}/>)}
         </div>
     );
 }
