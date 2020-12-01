322fd2cc63c5731097f2021b8b4f9dc6c2d8843f
h3park <h3park@ucsd.edu>
2020-12-01 10:40:53 -0800

    edit index list

--- src/components/index-list/index-list.component.jsx
+++ src/components/index-list/index-list.component.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import './index-list.style.css'
 
 export const IndexList = (props) => {
     return(
-        <div className='index-list'>{props.children}</div>
+        <div className='index-list'>
+            {props.dramas.map(drama => <h1 key={drama.id}>{drama.name}</h1>)}
+        </div>
     );
 }
