54f98764ac5663ad8cee20c715160022359813c6
h3park <h3park@ucsd.edu>
2020-12-01 13:54:45 -0800

    image added

--- src/components/index/index.component.jsx
+++ src/components/index/index.component.jsx
@@ -4,6 +4,9 @@ import './index.style.css'
 export const Index = (props) =>{
     return(
         <div className='index-container'>
+            {console.log(props.drama.poster_path)}
+            <img src={`https://image.tmdb.org/t/p/w200${props.drama.poster_path}`}/>
+            {/* <Image src={`https://image.tmdb.org/t/p/w500${props.drama.poster_path}`}  width={300} height={300}/> */}
             <h1>{props.drama.name}</h1>
         </div>
     );
